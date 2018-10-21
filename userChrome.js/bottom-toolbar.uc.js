7591614186de9af0f8e96911bf6904f10f882d8d
tortious <d_targaryen@mail.com>
2018-10-21 16:45:37 -0500

    Create bottom-toolbar.uc.js
    adds a bottom toolbar for addons, etc.

---
+++ userChrome.js/bottom-toolbar.uc.js
@@ -0,0 +1,26 @@
+//  bottomtoolbar.uc.js
+
+(function() {
+
+    if (location != 'chrome://browser/content/browser.xul')
+      return;
+  
+    let toolbar = document.createElement('toolbar');
+    toolbar.id = 'bottom-toolbar';
+    toolbar.setAttribute('customizable', 'true');
+    toolbar.setAttribute('mode', 'icons');
+    toolbar.setAttribute('context', 'toolbar-context-menu');
+  
+    let vbox  = document.createElement('vbox');
+    vbox.id = 'bottom-toolbar-vbox';
+    let browserBottombox = document.getElementById('browser-bottombox');
+    browserBottombox.parentNode.insertBefore(vbox, browserBottombox);
+    vbox.appendChild(toolbar);
+    vbox.style.backgroundColor = '#1f1f1f';
+    //Hier kann die Hintergrundfarbe angepasst werden. Als Wert kann zbs. red, green,
+    //yellow oder orange usw, aber auch zbs. #f7f7f7 usw.verwendet werden.
+  
+    CustomizableUI.registerArea('bottom-toolbar', {legacy: true});
+  
+  })();
+  
